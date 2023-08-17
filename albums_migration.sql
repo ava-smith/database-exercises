f206ce5eb1bc2cff42378996213e23eeeb276226
Ava Smith <ava013005@gmail.com>
2023-08-17 15:31:23 -0500

    finished writing sql

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date INT UNSIGNED,
+    sales INT UNSIGNED,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+)
