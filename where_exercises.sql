f1054b52867e41f331a0749ce624cb4e01a5a1f6
Ava Smith <ava013005@gmail.com>
2023-08-18 16:40:09 -0500

    finished part 1

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,7 +4,28 @@ SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT * FROM employees
-WHERE first_name LIKE 'E%';
+WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees
 WHERE last_name LIKE '%q%';
+
+
+SELECT * FROM employees
+WHERE first_name = 'Irena'
+    AND gender = 'M'
+    OR first_name = 'Vidya'
+    AND gender = 'M'
+    OR first_name = 'Maya'
+    AND gender = 'M';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name NOT LIKE '%qu%'
+    AND last_name LIKE '%q%';
