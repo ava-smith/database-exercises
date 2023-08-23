0f161f912c0e11eddc8478848c9101ef65bde646
Ava Smith <ava013005@gmail.com>
2023-08-23 10:37:42 -0500

    added bonus solution

--- join_exercises.sql
+++ join_exercises.sql
@@ -30,7 +30,9 @@ WHERE de.dept_no = 'd009'
   AND t.to_date = '9999-01-01'
 GROUP BY t.title;
 
-SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager", s.salary AS Salary
+SELECT d.dept_name                            AS "Department Name",
+       CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager",
+       s.salary                               AS Salary
 FROM employees e
          JOIN dept_manager dm
               ON dm.emp_no = e.emp_no
@@ -42,3 +44,21 @@ WHERE dm.to_date = '9999-01-01'
   AND s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
+#BONUS
+
+SELECT CONCAT(e.first_name, ' ', e.last_name)               AS Employee,
+       d.dept_name                                          AS Department,
+       CONCAT(managers.first_name, ' ', managers.last_name) AS Manager
+FROM employees e
+         JOIN dept_emp de
+              ON e.emp_no = de.emp_no
+         JOIN departments d
+              ON de.dept_no = d.dept_no
+         JOIN dept_manager dm
+              ON d.dept_no = dm.dept_no
+         JOIN employees managers
+              ON dm.emp_no = managers.emp_no
+WHERE de.to_date = '9999-01-01'
+  AND dm.to_date = '9999-01-01'
+ORDER BY d.dept_name, e.emp_no
+;
